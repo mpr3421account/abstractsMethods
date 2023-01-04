1384ee0cf4dfae2f09acf272fb853a0db45393c2
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-04 17:54:37 -0300

    Class Rectangle.

---
+++ Entities/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using abstractsMethods.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractsMethods.Entities
+{
+    internal class Rectangle : Shape
+    {
+        public Rectangle(Color color, double width, double height) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+
+        public override double Area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Entities/Shape.cs
+++ Entities/Shape.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using abstractsMethods.Entities.Enums;
 
 namespace abstractsMethods.Entities
 {
-    internal class Shape
+    internal abstract class Shape//devido o método Area ser abstrato, a classe também deve ser abstrata
     {
+        public Color Color { get; set; }
+
+        
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();//Area de shape é muito genérico, portanto devemos alterar para método abstrato
     }
 }
